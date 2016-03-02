700149504ab914b9433cfbb371329544ed699e4a
4jchc <jiangjin5y24@163.com>
2016-03-02 15:55:22 +0800

    【4jchc-MenuController】0.3 自定义菜单

--- 4jchc-MenuController/4jchc-MenuController/ViewController.swift
+++ 4jchc-MenuController/4jchc-MenuController/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: XMGLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "labelClick")
+        self.label.addGestureRecognizer(tap)
     }
 
 
+    func labelClick(){
+        // 1.label要成为第一响应者(作用是:告诉UIMenuController支持哪些操作, 这些操作如何处理)
+        self.becomeFirstResponder()
+        // 2.显示MenuController
+        let menu:UIMenuController = UIMenuController.sharedMenuController()
+        // 添加MenuItem
+        
+        let ding = UIMenuItem(title: "复制", action: Selector("ding:"))
+        let replay = UIMenuItem(title: "剪切", action: Selector("replay:"))
+        let report = UIMenuItem(title: "粘贴", action: Selector("report:"))
+        
+        menu.menuItems = [ding, replay, report]
+        // targetRect: MenuController需要指向的矩形框
+        // targetView: targetRect会以targetView的左上角为坐标原点
+        menu.setTargetRect(self.label.frame, inView: self.label.superview!)
+        
+        menu.setMenuVisible(true, animated: true)
+    }
+    
+    // 将自己的文字复制到粘贴板
+     func ding(menu: AnyObject?) {
+        let pboard = UIPasteboard.generalPasteboard()
+        pboard.string = self.label.text
+    }
+    
+    // 将自己的文字复制到粘贴板
+     func replay(sender: AnyObject?) {
+        // 将自己的文字复制到粘贴板
+        copy(sender)
+        // 清空文字
+        self.label.text = nil
+    }
+    
+     func report(sender: AnyObject?) {
+        // 将粘贴板的文字 复制 到自己身上
+        let pboard = UIPasteboard.generalPasteboard()
+        self.label.text = pboard.string
+    }
+    
 }
 
